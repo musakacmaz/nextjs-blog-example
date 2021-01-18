0508b4ebe3d38ab23313844355271d541736e378
Musa Kaçmaz <musakacmaz8@gmail.com>
2021-01-18 23:33:31 +0300

    Pre-rendering, Data fetching and Dynamic routes added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Head from "next/head";
+import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
+import Date from "../components/date";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return { props: { allPostsData } };
+}
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -18,10 +25,22 @@ export default function Home() {
           MongoDB, PHP and MySQL. Currently, I'm a master student at Dokuz Eylül
           University Computer Engineering Department.
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
